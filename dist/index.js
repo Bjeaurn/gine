08edd0ae3466b47a58d061123e8de91eb5f9e7c1
Bjorn S <bjorn@somnu.com>
2017-11-29 18:21:28 +0100

    Adds typings.

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,18 @@
+define(["require", "exports", "./gine/config", "./gine/core", "./gine/canvas", "./gine/handle", "./gine/camera", "./gine/image", "./gine/keyboard", "./gine/mouse", "./gine/text", "./gine/tile"], function (require, exports, config_1, core_1, canvas_1, handle_1, camera_1, image_1, keyboard_1, mouse_1, text_1, tile_1) {
+    "use strict";
+    function __export(m) {
+        for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    }
+    Object.defineProperty(exports, "__esModule", { value: true });
+    __export(config_1);
+    __export(core_1);
+    __export(canvas_1);
+    __export(handle_1);
+    __export(camera_1);
+    __export(image_1);
+    __export(keyboard_1);
+    __export(mouse_1);
+    __export(text_1);
+    __export(tile_1);
+});
 //# sourceMappingURL=index.js.map
