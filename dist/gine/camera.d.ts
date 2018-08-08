6ee4847324fd07376a93a3a0d0d6adb587d883a2
Bjorn S <bjorn@somnu.com>
2018-08-08 23:30:56 +0200

    Fixes cleaned up files

--- dist/gine/camera.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Camera {
-    private x;
-    private y;
-    constructor();
-    draw(): void;
-    update(): void;
-}
--- dist/gine/tile.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Tile {
-    readonly x: number;
-    readonly y: number;
-    draw(): void;
-}
