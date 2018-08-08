9d1a788c0da69009b930615d123377efca389666
Bjorn S <bjorn@somnu.com>
2018-08-08 23:25:54 +0200

    Cleans up errors in config

--- src/gine/config.ts
+++ src/gine/config.ts
@@ -1,13 +1,23 @@
 export class Config {
-    constructor(readonly width: number, readonly height: number, canvas: HTMLCanvasElement, readonly tickRate: number = 105,readonly  maxFps: number = 60, usesTiles: boolean = true, tileSize: number = 16) {}
+  usesTiles: boolean;
+  tileSize: number;
+  constructor(
+    readonly width: number,
+    readonly height: number,
+    readonly canvas: HTMLCanvasElement | null,
+    readonly tickRate: number = 105,
+    readonly maxFps: number = 60,
+    usesTiles: boolean = true,
+    tileSize: number = 16
+  ) {}
 }
 
 export const CONFIG: Config = {
-    maxFps: 60,
-    tickRate: 105,
-    width: 800,
-    height: 600,
-    usesTiles: true,
-    tileSize: 16,
-    canvas: null
-}
+  maxFps: 60,
+  tickRate: 105,
+  width: 800,
+  height: 600,
+  usesTiles: true,
+  tileSize: 16,
+  canvas: null
+};
