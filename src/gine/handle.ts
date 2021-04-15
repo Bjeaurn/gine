2181848820f6bb1758e953e57d6d4d0bc54e2eaa
Bjorn 'Bjeaurn <bjorn@somnu.com>
2021-04-15 20:14:29 +0200

    fix: Clears up the entire screen instead of just canvas bounds,

--- src/gine/handle.ts
+++ src/gine/handle.ts
@@ -1,81 +1,85 @@
 import { Canvas } from './canvas'
-import { Config } from './config'
 import { Gine } from './core'
-import { Asset, ImageAsset, SpriteAsset } from './image'
+import { Asset, SpriteAsset } from './image'
 import { Font } from './text'
 
 export class Handle {
-  public readonly handle: CanvasRenderingContext2D
-  constructor(private canvas: Canvas) {
-    this.handle = this.canvas.canvasElm.getContext(
-      '2d'
-    ) as CanvasRenderingContext2D
-    this.scale(this.canvas.scale)
-  }
-
-  public clear() {
-    this.handle.clearRect(-1, -1, Gine.CONFIG.width + 1, Gine.CONFIG.height + 1)
-  }
-
-  public text(value: string | number, x: number, y: number) {
-    this.handle.fillText(value as string, x, y)
-  }
-
-  public setFont(font: Font) {
-    this.handle.font = font.toString()
-  }
-
-  public setColor(red: number, green: number, blue: number, alpha?: number) {
-    if (!alpha) {
-      alpha = 1.0
+    public readonly handle: CanvasRenderingContext2D
+    constructor(private canvas: Canvas) {
+        this.handle = this.canvas.canvasElm.getContext(
+            '2d'
+        ) as CanvasRenderingContext2D
+        this.scale(this.canvas.scale)
     }
-    this.handle.fillStyle =
-      'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')'
-  }
 
-  public draw(image: Asset, x: number, y: number) {
-    if (
-      x < Gine.CONFIG.viewport.maxX &&
-      x > Gine.CONFIG.viewport.minX &&
-      y < Gine.CONFIG.viewport.maxY &&
-      y > Gine.CONFIG.viewport.minY
+    public clear() {
+        this.handle.clearRect(-1, -1, window.innerWidth, window.innerHeight)
+    }
+
+    public text(value: string | number, x: number, y: number) {
+        this.handle.fillText(value as string, x, y)
+    }
+
+    public setFont(font: Font) {
+        this.handle.font = font.toString()
+    }
+
+    public setColor(red: number, green: number, blue: number, alpha?: number) {
+        if (!alpha) {
+            alpha = 1.0
+        }
+        this.handle.fillStyle =
+            'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')'
+    }
+
+    public draw(image: Asset, x: number, y: number) {
+        if (
+            x < Gine.CONFIG.viewport.maxX &&
+            x > Gine.CONFIG.viewport.minX &&
+            y < Gine.CONFIG.viewport.maxY &&
+            y > Gine.CONFIG.viewport.minY
+        ) {
+            this.handle.drawImage(image.image, x, y)
+        }
+    }
+
+    public drawSprite(
+        image: SpriteAsset,
+        x: number,
+        y: number,
+        index?: number
     ) {
-      this.handle.drawImage(image.image, x, y)
+        if (index) {
+            image.calculatePerIndex(index)
+        } else {
+            image.draw()
+        }
+        this.handle.drawImage(
+            image.image,
+            image.sourceX,
+            image.sourceY,
+            image.sizeX,
+            image.sizeY,
+            x,
+            y,
+            image.sizeX,
+            image.sizeY
+        )
     }
-  }
 
-  public drawSprite(image: SpriteAsset, x: number, y: number, index?: number) {
-    if (index) {
-      image.calculatePerIndex(index)
-    } else {
-      image.draw()
+    public rotateImage(image: Asset, degrees?: number) {
+        if (!degrees) {
+            degrees = 0
+        }
+        const radians = (degrees * Math.PI) / 180
+        // COME BACK TO THIS LATER, FIGURE OUT THE `BUFFERHANDLE`
     }
-    this.handle.drawImage(
-      image.image,
-      image.sourceX,
-      image.sourceY,
-      image.sizeX,
-      image.sizeY,
-      x,
-      y,
-      image.sizeX,
-      image.sizeY
-    )
-  }
 
-  public rotateImage(image: Asset, degrees?: number) {
-    if (!degrees) {
-      degrees = 0
+    public resetColor() {
+        this.setColor(0, 0, 0)
     }
-    const radians = (degrees * Math.PI) / 180
-    // COME BACK TO THIS LATER, FIGURE OUT THE `BUFFERHANDLE`
-  }
 
-  public resetColor() {
-    this.setColor(0, 0, 0)
-  }
-
-  public scale(scale: number) {
-    this.handle.scale(scale, scale)
-  }
+    public scale(scale: number) {
+        this.handle.scale(scale, scale)
+    }
 }
